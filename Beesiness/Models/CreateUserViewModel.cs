83449c13e2bbcb4c423ccabd2c72b26f58af8f15
jevolutioncl <shelburneofficial@gmail.com>
2023-10-25 11:06:51 -0300

    25-10-2023

---
+++ Beesiness/Models/CreateUserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Beesiness.Models
+{
+    public class CreateUserViewModel
+    {
+        public string Nombre { get; set; }
+        public string Email { get; set; } 
+        public string Password { get; set; }  
+        public int IdRolSeleccionado { get; set; }
+        
+        [BindNever]
+        public IEnumerable<SelectListItem> RolesDisponibles { get; set; }
+
+    }
+}
---
+++ Beesiness/Models/UsuarioTemporal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Beesiness.Models
+{
+    public class UsuarioTemporal
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Correo { get; set; }
+
+        [Required]
+        public string Rol { get; set; }
+
+        public DateTime FechaSolicitud { get; set; }
+
+        public UsuarioTemporal()
+        {
+            FechaSolicitud = DateTime.Now;
+        }
+    }
+}
