e4fdba25745f534980d86157c15fe1d047d89abd
lazoels <eliaslazo@gmail.com>
2023-10-25 11:42:06 -0300

    nuevo controlador

---
+++ Beesiness/Controllers/ColmenaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Beesiness.Controllers
+{
+    public class ColmenaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
