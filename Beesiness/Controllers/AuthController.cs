7613f4de74aaab1aba7b056f089fb193d39509d9
jevolutioncl <shelburneofficial@gmail.com>
2023-10-30 21:48:02 -0300

    - Se agregó que el sistema detecte cuando el usuario ingrese en el formulario de solicitud de registro que sean datos válidos, requeridos o que el correo no esté duplicado o usado con anterioridad. - Se agregaron validaciones en el Login, se le mostrará al usuario cuando no fue encontrado, contraseña incorrecta, correo incorrecto o que ingrese los datos requeridos. - Se agregó funcionalidad a “¿olvidaste tu contraseña?”

--- Beesiness/Controllers/AuthController.cs
+++ Beesiness/Controllers/AuthController.cs
@@ -18,15 +18,76 @@ namespace Beesiness.Controllers
         {
             _context = context;
         }
+
         [Authorize(Roles = "Root")]
-        public async Task<IActionResult> RequestRegistrationIndex()
+        public async Task<IActionResult> RequestRegistrationIndex(string sortOrder)
         {
             if (User.Identity.IsAuthenticated)
             {
-                var requestUsers = await _context.tblUsuariosTemporales.ToListAsync();
-                return View(requestUsers);
+                // Criterios de ordenamiento
+                ViewBag.CurrentSort = sortOrder;
+                ViewBag.IdSort = String.IsNullOrEmpty(sortOrder) ? "id_desc" : "";
+                ViewBag.NombreSort = sortOrder == "nombre" ? "nombre_desc" : "nombre";
+                ViewBag.CorreoSort = sortOrder == "correo" ? "correo_desc" : "correo";
+                ViewBag.RolSort = sortOrder == "rol" ? "rol_desc" : "rol";
+                ViewBag.FechaSort = sortOrder == "fecha" ? "fecha_desc" : "fecha";
+
+                var usuarios = from u in _context.tblUsuariosTemporales
+                               select u;
+
+                switch (sortOrder)
+                {
+                    case "id_desc":
+                        usuarios = usuarios.OrderByDescending(u => u.Id);
+                        break;
+                    case "nombre":
+                        usuarios = usuarios.OrderBy(u => u.Nombre);
+                        break;
+                    case "nombre_desc":
+                        usuarios = usuarios.OrderByDescending(u => u.Nombre);
+                        break;
+                    case "correo":
+                        usuarios = usuarios.OrderBy(u => u.Correo);
+                        break;
+                    case "correo_desc":
+                        usuarios = usuarios.OrderByDescending(u => u.Correo);
+                        break;
+                    case "rol":
+                        usuarios = usuarios.OrderBy(u => u.Rol);
+                        break;
+                    case "rol_desc":
+                        usuarios = usuarios.OrderByDescending(u => u.Rol);
+                        break;
+                    case "fecha":
+                        usuarios = usuarios.OrderBy(u => u.FechaSolicitud);
+                        break;
+                    case "fecha_desc":
+                        usuarios = usuarios.OrderByDescending(u => u.FechaSolicitud);
+                        break;
+                    default:
+                        usuarios = usuarios.OrderBy(u => u.Id);
+                        break;
+                }
+
+                var viewModel = new RequestRegistrationIndexViewModel
+                {
+                    Usuarios = await usuarios.ToListAsync(),
+                    CurrentSort = sortOrder,
+                    IdSort = ViewBag.IdSort,
+                    NombreSort = ViewBag.NombreSort,
+                    CorreoSort = ViewBag.CorreoSort,
+                    RolSort = ViewBag.RolSort,
+                    FechaSort = ViewBag.FechaSort
+                };
+
+                return View(viewModel);
             }
             return RedirectToAction("LoginIn", "Auth");
+        }
+
+        public IActionResult ContraseñaOlvidada()
+        {
+            return View();
         }
         public IActionResult RequestRegistration()
         {
@@ -36,21 +97,33 @@ namespace Beesiness.Controllers
         [HttpPost]
         public async Task<IActionResult> RequestRegistration(LoginRegistrationViewModel model)
         {
-            // Mapear el ViewModel a la entidad UsuarioTemporal
-            var usuarioTemporal = new UsuarioTemporal
+            if (!ModelState.IsValid)
             {
-                Nombre = model.Registration.NombreCompleto,
-                Correo = model.Registration.Correo,
-                Rol = model.Registration.RolSeleccionado
-            };
+                var errors = ModelState.SelectMany(x => x.Value.Errors.Select(p => p.ErrorMessage)).ToList();
+                var existingTempUser = await _context.tblUsuariosTemporales.FirstOrDefaultAsync(u => u.Correo == model.Registration.Correo);
+                var existingUser = await _context.tblUsuarios.FirstOrDefaultAsync(u => u.Correo == model.Registration.Correo);
 
-            // Guardar en la base de datos
-            _context.tblUsuariosTemporales.Add(usuarioTemporal);
-            await _context.SaveChangesAsync();
+                if (existingTempUser != null || existingUser != null)
+                {
+                    ModelState.AddModelError("Registration.Correo", "Este correo ya está registrado.");
+                    return View("LoginIn", model);
+                }
 
-            // Redirigir a una página de confirmación o donde quieras después de registrar al usuario
-            return RedirectToAction("LoginIn", "Auth");
+                var usuarioTemporal = new UsuarioTemporal
+                {
+                    Nombre = model.Registration.NombreCompleto,
+                    Correo = model.Registration.Correo,
+                    Rol = model.Registration.RolSeleccionado
+                };
+
+                _context.tblUsuariosTemporales.Add(usuarioTemporal);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("LoginIn", "Auth");
+            }
+
+            return View("LoginIn", model); // Si no es válido o hay un error, regresamos el modelo a la vista.
         }
+
 
         [HttpGet]
         public IActionResult LoginIn(Usuario U)
