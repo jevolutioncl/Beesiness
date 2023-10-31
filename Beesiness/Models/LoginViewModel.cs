57abb3aedfafe2bf024bd58ac84dd0f4e0fcfe47
jevolutioncl <shelburneofficial@gmail.com>
2023-10-30 21:48:02 -0300

    - Se agregó que el sistema detecte cuando el usuario ingrese en el formulario de solicitud de registro que sean datos válidos, requeridos o que el correo no esté duplicado o usado con anterioridad. - Se agregaron validaciones en el Login, se le mostrará al usuario cuando no fue encontrado, contraseña incorrecta, correo incorrecto o que ingrese los datos requeridos. - Se agregó funcionalidad a “¿olvidaste tu contraseña?”

--- Beesiness/Models/LoginViewModel.cs
+++ Beesiness/Models/LoginViewModel.cs
@@ -4,22 +4,25 @@ namespace Beesiness.Models
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "El correo es requerido")]
+        [EmailAddress(ErrorMessage ="El correo no existe")]
         public string Correo { get; set; }
+        [Required(ErrorMessage = "La contraseña es requerida")]
         public string Password { get; set; }
 
     }
     public class RegistrationRequestViewModel
     {
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "El correo es requerido.")]
+        [EmailAddress(ErrorMessage = "El formato del correo es inválido")]
         [Display(Name = "Correo electrónico")]
         public string Correo { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El nombre es requerido.")]
         [Display(Name = "Nombre y apellido")]
         public string NombreCompleto { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El rol es requerido.")]
         // Si decides agregar roles:
         [Display(Name = "Rol")]
         public string RolSeleccionado { get; set; } // para almacenar el rol seleccionado por el usuario
---
+++ Beesiness/Models/RequestRegistrationIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Beesiness.Models
+{
+    public class RequestRegistrationIndexViewModel
+    {
+        public List<UsuarioTemporal> Usuarios { get; set; }
+        public string CurrentSort { get; set; } // Actual criterio de ordenamiento
+        public string NombreSort { get; set; }
+        public string CorreoSort { get; set; }
+        public string RolSort { get; set; }
+        public string FechaSort { get; set; }
+        public string IdSort { get; set; }
+    }
+
+
+
+}
